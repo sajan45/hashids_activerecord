05d2c9b5616876ef695f6eca679b0f96caaea63d
sajan <sajan.kumar@hiddenbrains.in>
2017-02-23 11:54:31 +0530

    Removed 'find' override

--- lib/hashids_activerecord.rb
+++ lib/hashids_activerecord.rb
@@ -25,22 +25,6 @@ module HashidsActiverecord
   end
 
   module ClassMethods
-    def find(*args)
-      scope = args.slice!(0)
-      options = args.slice!(0) || {}
-      if has_hashed_id? && !options[:no_hashed_id]
-        if scope.is_a?(Array)
-          scope.map! {|a| dehash_id(a).to_i}
-        else
-          scope = dehash_id(scope)
-        end
-      end
-      super(scope)
-    end
-
-    def has_hashed_id?
-      true
-    end
 
     def dehash_id(hashed_id)
       HashidsActiverecord.show(hashed_id, self.hash_salt, self.min_length, self.alphabets)
@@ -63,25 +47,6 @@ module HashidsActiverecord
                                                    self.class.min_length,
                                                    self.class.alphabets)
       self.save
-    end
-    # Override ActiveRecord::Persistence#reload
-    # passing in an options flag with { no_hashed_id: true }
-    def reload(options = nil)
-      options = (options || {}).merge(no_hashed_id: true)
-
-      clear_aggregation_cache
-      clear_association_cache
-
-      fresh_object =
-        if options && options[:lock]
-          self.class.unscoped { self.class.lock(options[:lock]).find(id, options) }
-        else
-          self.class.unscoped { self.class.find(id, options) }
-        end
-
-      @attributes = fresh_object.instance_variable_get('@attributes')
-      @new_record = false
-      self
     end
   end
 end
--- lib/hashids_activerecord/version.rb
+++ lib/hashids_activerecord/version.rb
@@ -1,3 +1,3 @@
 module HashidsActiverecord
-  VERSION = "0.0.2"
+  VERSION = "0.1.2"
 end
